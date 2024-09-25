747930997d96411ebd86f6d95f4d3efb409c686f
naomiHauret <hauretnaomi@gmail.com>
2024-09-25 15:18:51 +0200

    chore: bootstrap backend  (elysia)

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,17 @@
+import { Elysia, t } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
+
+const app = new Elysia()
+  .use(cors())
+  .use(swagger())
+  .get("/ping", () => () => ({
+    message: "Pong!"
+  }))
+  .listen(process.env.PORT ?? 3000);
+
+console.log(
+  `Elysia server running at ${app.server?.hostname}:${app.server?.port}`
+);
+
+export type App = typeof app;
